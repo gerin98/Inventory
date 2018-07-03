ec873335ecb35d8344eb3beb8643dadfc8e3fc42
gerin98 <gerin.amalaraj@mail.utoronto.ca>
2018-07-03 18:37:32 -0400

    "added menu items to editor activity"

--- app/src/main/java/com/example/gerin/inventory/CatalogActivity.java
+++ app/src/main/java/com/example/gerin/inventory/CatalogActivity.java
@@ -1,5 +1,7 @@
 package com.example.gerin.inventory;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,9 +22,19 @@ public class CatalogActivity extends AppCompatActivity {
         // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
         View emptyView = findViewById(R.id.empty_view);
         petListView.setEmptyView(emptyView);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.catalog_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    /* methods to create menu */
+    /* Methods to create menu */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
@@ -37,9 +49,15 @@ public class CatalogActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
+                deleteAllItems();
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /* Method to delete all items in the database */
+    private void deleteAllItems(){
+
     }
 
 
--- app/src/main/java/com/example/gerin/inventory/EditorActivity.java
+++ app/src/main/java/com/example/gerin/inventory/EditorActivity.java
@@ -1,7 +1,11 @@
 package com.example.gerin.inventory;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -9,5 +13,36 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        // Examine the intent that was used to launch this activity
+        Intent intent = getIntent();
+
+        // Assume for now that user is inserting a new pet
+        setTitle(getString(R.string.editor_activity_title_new_pet));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Delete entry" menu option
+            case R.id.action_delete_entry:
+                return true;
+            case android.R.id.home:
+                // Navigate up to parent activity
+                // Show a dialog later on asking if user really wants to leave
+                NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
