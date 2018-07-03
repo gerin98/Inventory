ab650df81f5baf42d4da88bd0153274a2727a77b
gerin98 <gerin.amalaraj@mail.utoronto.ca>
2018-07-03 18:10:53 -0400

    "added empty view to listview in catalog activity"

--- app/src/main/java/com/example/gerin/inventory/CatalogActivity.java
+++ app/src/main/java/com/example/gerin/inventory/CatalogActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -11,6 +13,13 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        // Find the ListView which will be populated with the pet data
+        ListView petListView = (ListView) findViewById(R.id.catalog_list);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        petListView.setEmptyView(emptyView);
     }
 
     /* methods to create menu */
