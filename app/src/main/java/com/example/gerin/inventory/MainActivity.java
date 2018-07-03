589e66a23f05bb6c8cd09c0a12ff81c960cca9c4
gerin98 <gerin.amalaraj@mail.utoronto.ca>
2018-07-03 15:40:35 -0400

    "updated splash screen"

--- app/src/main/java/com/example/gerin/inventory/MainActivity.java
+++ app/src/main/java/com/example/gerin/inventory/MainActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+/* This file is used to create a splash screen */
 public class MainActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 4000;
@@ -18,8 +20,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView logo = (ImageView) findViewById(R.id.splash_screen_logo);
+        TextView name = (TextView) findViewById(R.id.splash_screen_name);
+
+
+        //  create animations from resources
         Animation fromBottom = AnimationUtils.loadAnimation(this,R.anim.from_bottom);
+        Animation fadeIn = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+
+        //  apply animations
         logo.setAnimation(fromBottom);
+        name.setAnimation(fadeIn);
+
+        // delay for app name to appear
+        fadeIn.setStartOffset(2000);
 
         new Handler().postDelayed(new Runnable() {
             @Override
