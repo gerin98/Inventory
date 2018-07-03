5cd6a5f3d55863061d4aa3d8c6c531123154e43e
gerin98 <gerin.amalaraj@mail.utoronto.ca>
2018-07-03 17:04:27 -0400

    "added menu items in catalog activity"

--- app/src/main/java/com/example/gerin/inventory/CatalogActivity.java
+++ app/src/main/java/com/example/gerin/inventory/CatalogActivity.java
@@ -2,6 +2,8 @@ package com.example.gerin.inventory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -10,4 +12,27 @@ public class CatalogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
     }
+
+    /* methods to create menu */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/gerin/inventory/EditorActivity.java
@@ -0,0 +1,13 @@
+package com.example.gerin.inventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+}
