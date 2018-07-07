5ae2b8c0e9e47c6bc6678cc1e503abd92cf11714
gerin98 <gerin.amalaraj@mail.utoronto.ca>
2018-07-07 12:50:55 -0400

    "updated schema"

--- app/src/main/java/com/example/gerin/inventory/data/ItemDbHelper.java
+++ app/src/main/java/com/example/gerin/inventory/data/ItemDbHelper.java
@@ -3,6 +3,7 @@ package com.example.gerin.inventory.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 
 import com.example.gerin.inventory.data.ItemContract.ItemEntry;
@@ -30,16 +31,20 @@ public class ItemDbHelper extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        Log.e("ItemDbHelper","inside onCreate");
         // Create a String that contains the SQL statement to create the pets table
         String SQL_CREATE_ITEMS_TABLE = "CREATE TABLE " + ItemEntry.TABLE_NAME + " ("
                 + ItemEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + ItemEntry.COLUMN_ITEM_NAME + " TEXT NO NULL, "
-                + ItemEntry.COLUMN_ITEM_PRICE + " DOUBLE DEFAULT 0, "
+                + ItemEntry.COLUMN_ITEM_NAME + " TEXT NOT NULL, "
                 + ItemEntry.COLUMN_ITEM_QUANTITY + " DOUBLE DEFAULT 0, "
+                + ItemEntry.COLUMN_ITEM_PRICE + " DOUBLE DEFAULT 0, "
                 + ItemEntry.COLUMN_ITEM_DESCRIPTION + "TEXT, "
                 + ItemEntry.COLUMN_ITEM_TAG1 + "TEXT, "
                 + ItemEntry.COLUMN_ITEM_TAG2 + "TEXT, "
                 + ItemEntry.COLUMN_ITEM_TAG3 + "TEXT);";
+
+        // Execute the SQL statement
+        db.execSQL(SQL_CREATE_ITEMS_TABLE);
         /*
         String SQL_CREATE_PETS_TABLE =  "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
                 + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
@@ -52,6 +57,6 @@ public class ItemDbHelper extends SQLiteOpenHelper{
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        Log.e("ItemDbHelper","inside onUpgrade");
     }
 }
---
+++ app/src/main/java/com/example/gerin/inventory/data/ItemProvider.java
@@ -0,0 +1,5 @@
+package com.example.gerin.inventory.data;
+
+public class ItemProvider {
+
+}
